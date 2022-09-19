6de1555c5a17a406840825dba787998a7d064e66
Joseph Whom <itswhom@gmail.com>
2022-09-18 21:31:44 -0700

    add png conversion script and output

--- python-ztm/17_scripting_with_python/image_processing/JPGtoPNGconverter.py
+++ python-ztm/17_scripting_with_python/image_processing/JPGtoPNGconverter.py
@@ -1,14 +1,24 @@
+from lib2to3.pytree import convert
 from PIL import Image
 import sys
 import os
 import pathlib
 
-# name.py Pokedex/ new/
+# the run line:
+# python3 name.py Pokedex/ new/
 
 # grab first and second argument
+source_dir = str(sys.argv[1])
+destination_dir = str(sys.argv[2])
 
 # check if new/ exists, if not create it
+if not os.path.exists(destination_dir):
+    os.mkdir(destination_dir)
 
 # loop through pokedex/
-# convert images to png
-# save to new folder
+for mon in os.listdir(source_dir):
+    source_image = Image.open(source_dir + mon)
+    stem = pathlib.Path(mon).stem
+    # save to new folder as png
+    source_image.save(destination_dir + stem + '.png')
+    
